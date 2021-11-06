cb06cb658f07e7e55925379254d86ada6be98766
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2021-11-06 16:33:11 +0900

    Chapter 4-7 Finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if User.find_by(email: 'admin@example.com').nil?
+  User.create(name: 'admin', email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin: true)
+  User.create(name: 'user', email: 'user@example.com', password: 'password', password_confirmation: 'password', admin: false)
+end
